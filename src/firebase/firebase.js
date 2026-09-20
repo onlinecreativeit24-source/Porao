af014c6b3bbea719c7496bad2d9187c370e6096c
onlinecreativeit24-source <256857795+onlinecreativeit24-source@users.noreply.github.com>
2026-09-20 12:35:15 +0600

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,1 +1,17 @@
 
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBirgAkj9gbGYKjcUSsCFYLIJ6IoXbMK0E",
+  authDomain: "porao-dd00d.firebaseapp.com",
+  projectId: "porao-dd00d",
+  storageBucket: "porao-dd00d.firebasestorage.app",
+  messagingSenderId: "251968287983",
+  appId: "1:251968287983:web:3d8f51cd438dbb6147171a"
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
